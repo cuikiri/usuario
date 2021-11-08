ac1c7d0e47c29e47b043957c2580a56143963b14
cuikiri <cuikiri@gmail.com>
2021-11-07 23:23:13 -0300

    Ajustes para SFH

--- src/main/webapp/app/shared/layout/header/header-components.tsx
+++ src/main/webapp/app/shared/layout/header/header-components.tsx
@@ -9,7 +9,7 @@ import appConfig from 'app/config/constants';
 
 export const BrandIcon = props => (
   <div {...props} className="brand-icon">
-    <img src="content/images/logo-jhipster.png" alt="Logo" />
+    <img src="content/images/logo-sfh.png" alt="Logo" />
   </div>
 );
 
@@ -17,7 +17,7 @@ export const Brand = props => (
   <NavbarBrand tag={Link} to="/" className="brand-logo">
     <BrandIcon />
     <span className="brand-title">
-      <Translate contentKey="global.title">Users</Translate>
+      
     </span>
     <span className="navbar-version">{appConfig.VERSION}</span>
   </NavbarBrand>
